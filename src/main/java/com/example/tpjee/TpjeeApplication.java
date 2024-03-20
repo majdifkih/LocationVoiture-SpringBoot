53c46e0428efaad104d715d7fcecfde666afe78e
majdi <majdi@majdi.com>
2024-03-20 14:41:44 +0100

    first commit

---
+++ src/main/java/com/example/tpjee/TpjeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpjeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpjeeApplication.class, args);
+    }
+
+}
